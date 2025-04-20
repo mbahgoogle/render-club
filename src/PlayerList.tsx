70ebb76b225a2a6c309e16f43efef163b396f452
ass <jurus32@gmail.com>
2025-04-20 15:34:25 +0700

    newcastle_united

--- src/PlayerList.tsx
+++ src/PlayerList.tsx
@@ -33,8 +33,8 @@ import { getLogoCode } from "./utils/getLogoClub";
 // import rawTopPlayers from "../public/en/leicester_city_top_goals.json";
 // import rawTopPlayers from "../public/en/liverpool_fc_top_goals.json";
 // import rawTopPlayers from "../public/en/manchester_city_top_goals.json";
-import rawTopPlayers from "../public/en/manchester_united_top_goals.json";
-// import rawTopPlayers from "../public/en/newcastle_united_top_goals.json";
+// import rawTopPlayers from "../public/en/manchester_united_top_goals.json";
+import rawTopPlayers from "../public/en/newcastle_united_top_goals.json";
 
 // import rawTopPlayers from "../public/en/nottingham_forest_top_goals.json";
 // import rawTopPlayers from "../public/en/southampton_fc_top_goals.json";
